6a59d84d5fdfcf8a0389a2fdf66d396bc649b8de
Lily (Xianling) Z <alchemxz@gmail.com>
2018-06-04 15:14:32 -0700

    Update [Bit]Single Number.cpp

--- [Bit]Single Number.cpp
+++ [Bit]Single Number.cpp
@@ -11,3 +11,46 @@ public:
         return res;
     }
 };
+
+// Problem 2
+// a = [1, 1, 5, 5, 2, 2, 6, 4, 4, 3, 3]
+// a = [1, 1, 6, 5, 5, 2, 2, 4, 4, 3, 3]
+int findOne(vector<int>& nums){
+    int left=0, right=nums.size()-1;
+    while(left<=right){
+        int mid=left + (right-left)/2;//10/2
+        if(mid%2==1){
+            if(nums[mid]==nums[mid-1]){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }else{
+            if(nums[mid]==nums[mid-1]){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }
+    }
+    return right;
+}
+
+/*
+odd,
+nums[mid]==nums[mid-1], drop left，left=mid+1
+else, drop right，right=mid-1;
+even
+nums[mid]==nums[mid-1], right=mid+1
+else, left=mid+1
+
+left=0, right=10, mid=5, 
+odd, nums[mid]==nums[mid-1], left=6
+left=6, right=10, mid=8, 
+even, nums[mid]=nums[mid-1], right=7
+left=6, right=7, mid=6
+even, nums[mid]!=nums[mid-1], left=7
+left=7, right=7, mid=7, 
+odd, nums[mid]!=nums[mid-1], right=6
+
+*/
