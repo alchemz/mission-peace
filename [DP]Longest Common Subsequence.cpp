2906e7b0638ce6294b61c29001f1a76fbb04459d
Lily (Xianling) Z <alchemxz@gmail.com>
2018-06-04 13:06:01 -0700

    Update and rename [DP]Longest Common Sequence.cpp to [DP]Longest Common Subsequence.cpp

---
+++ [DP]Longest Common Subsequence.cpp
@@ -0,0 +1,20 @@
+int longestUtil(vector<int> a, vector<int> b){
+    int m=a.size();
+    int n=b.size();
+    vector<vector<int>> T(m+1, vector<int>(n+1));
+
+    for(int i=0; i<T.size(); i++){
+        for(int j=0; j<T[i].size();j++){
+            if(i==0 || j==0)
+                T[i][j]=0;
+            else if(a[i-1]==b[j-1]){
+                T[i][j]= 1 + T[i-1][j-1];
+            }
+            else{
+                T[i][j]= max(T[i][j-1], T[i-1][j]);
+            }
+        }
+    }
+
+    return T[m][n];
+}
