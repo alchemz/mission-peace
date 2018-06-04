ead92ce26b6ca00ce05eba4a3ec8a5c781a7b02c
Lily (Xianling) Z <alchemxz@gmail.com>
2018-06-04 11:53:09 -0700

    Update [DP]Subset with sum divisible by m.cpp

--- [DP]Subset with sum divisible by m.cpp
+++ [DP]Subset with sum divisible by m.cpp
@@ -1,36 +1,25 @@
-// C++ program to check if there is a subset
-// with sum divisible by m.
 #include <bits/stdc++.h>
 using namespace std;
 
 bool modularSum(vector<int> arr, int n, int m)
 {
-	if (n > m) return true;
-
+	//if (n > m) return true;
     vector<bool> DP(m, false);
-
 	for (int i=0; i<n; i++){
-		if (DP[0])
-			return true;
-
+		//if (DP[0]) return true;
 		vector<bool> temp(m, false);
-
 		for (int j=0; j<m; j++){
 			if (DP[j] == true){
-				if (DP[(j+arr[i]) % m] == false){
+				if (DP[(j+arr[i]) % m] == 0){
 					temp[(j+arr[i]) % m] = true;
 				}
 			}
 		}
-
 		for (int j=0; j<m; j++){
-			if (temp[j]){
-				DP[j] = true;
-			}
+			if (temp[j]) DP[j] = true;
 		}
 		DP[arr[i]%m] = true;
 	}
-
 	return DP[0];
 }
 
