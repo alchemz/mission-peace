f843376de832ee72eb153e356137a7b6ae3785cd
Lily (Xianling) Z <alchemxz@gmail.com>
2018-06-29 08:40:04 -0700

    Update [DP]322. Coin Change.cpp

--- [DP]322. Coin Change.cpp
+++ [DP]322. Coin Change.cpp
@@ -1,18 +1,24 @@
-// Non-recursion
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, amount + 1);
+        vector<int> dp(amount + 1,amount+1);
         dp[0] = 0;
-        for (int i = 1; i <= amount; ++i) {
-            for (int j = 0; j < coins.size(); ++j) {
-                if (coins[j] <= i) {
-                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+        for (int i = 0; i < coins.size(); ++i) {
+            for (int j = 1; j <= amount; ++j) { 
+                if (j>=coins[i]) { //需要3，有2的硬币
+                    dp[j] = min(dp[j], dp[j - coins[i]] + 1);
                 }
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+        if(dp[amount] > amount) 
+            return -1;
+        return dp[amount];
     }
 };
-// http://www.cnblogs.com/grandyang/p/5138186.html
-// https://leetcode.com/problems/coin-change/description/
+
+
+// equation:
+// if j>= coins[i]
+//     T[i][j] = T[i-1][j] + T[i][j-1];
+// else
+//     T[i][j] = T[i-1][j]
